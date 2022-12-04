c36381cd9dda105c117da2363e31ff9d44532134
cielo <114308552+cielobuezo@users.noreply.github.com>
2022-12-03 20:43:21 -0600

    Add files via upload

---
+++ Lista/cAlumnos.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lista_mysql;
+
+/**
+ *
+ * @author user
+ */
+public class cAlumnos {
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public string getNombrealumnos() {
+        return nombrealumnos;
+    }
+
+    public void setNombrealumno(string nombrealumnos) {
+        this.nombrealumno = nombrealumnos;
+    }
+
+    public string getApellidoalumnos() {
+        return apellidoalumnos;
+    }
+
+    public void setApellidoalumno(string apellidoalumno) {
+        this.apellidoalumno = apellidoalumno;
+    }
+    
+    int codigo;
+    string nombrealumno;
+    string apellidoalumno;
+    
+}
